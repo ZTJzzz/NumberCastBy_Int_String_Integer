1acd7711108c89873cd7a449fb4855d35ea4cd37
zhoutijian <845530038@qq.com>
2019-10-20 22:03:20 +0800

    数据型常量池的概念(-128 ~ 127)

--- NumberCastBy_Int_String_Integer.java
+++ NumberCastBy_Int_String_Integer.java
@@ -48,5 +48,37 @@ public class NumberCastBy_Int_String_Integer {
 		
 		//自动拆箱
 		int i7 = integer7;
+		
+		/**
+		 * 注意：
+		 * 首先 Integer integer8 = 128   等同于  Integer integer8 = new Integer(128);
+		 * 
+		 * 如果数据是在（-128 ~ 127）之间，Java引入了一个“整数型常量池”，在方法区中
+		 * 整数型常量池 只存储 -128 ~ 127 之间的数据，
+		 * 是因为这些数据最常用。
+		 * 
+		 * 另外，Integer类型的数据判断大小不能使用“==”, 应该使用equals()方法
+		 * 
+		 */
+		Integer integer8 = 128;
+		Integer integer9 = 128; 
+		System.out.println(integer8 == integer9);   //false
+		System.out.println(integer8.equals(integer9));//true
+		
+		Integer integer10 = 127;
+		Integer integer11 = 127; // 这里不再创建新的对象，而是从数据型常量池中直接获取
+		System.out.println(integer10 == integer11);   //true
+		System.out.println(integer10.equals(integer11));//true
+		
+		Integer integer12 = -128;
+		Integer integer13 = -128;// 这里不再创建新的对象，而是从数据型常量池中直接获取
+		System.out.println(integer12 == integer13);   //true
+		System.out.println(integer12.equals(integer13));//true
+		
+		Integer integer14 = -129;
+		Integer integer15 = -129;
+		System.out.println(integer14 == integer15);   //false
+		System.out.println(integer14.equals(integer15));//true
+		
 	}
 }
