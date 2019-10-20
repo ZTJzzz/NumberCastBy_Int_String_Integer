07919fa0db4301f2f7a65f2878c337e4aef6e0e4
zhoutijian <845530038@qq.com>
2019-10-20 21:24:34 +0800

    三种简单数据类型之间的转换

--- NumberCastBy_Int_String_Integer.java
+++ NumberCastBy_Int_String_Integer.java
@@ -1,19 +1,39 @@
 package Int_String_Integer之间类型转换;
 
+/**
+ * 
+ * 三种最常见数据类型之间的转换（int - String - Integer）
+ * 可详见JavaAPI文档
+ * @author ztj
+ *
+ */
 public class NumberCastBy_Int_String_Integer {
 
 	public static void main(String[] args) {
 		
 		//int --> String
+		int i1 =10;
+		String s1 = i1 + "";
 		
-		//int --> String
+		//String --> int
+		String s2 = "123";
+		int i2 = Integer.parseInt(s2);
 		
-		//int --> String
+		//int --> Integer
+		int i3 = 10;
+		Integer integer3 = Integer.valueOf(i3);
 		
-		//int --> String
+		//Integer --> int
+		Integer integer4 = new Integer(10);
+		int i4 = integer4.intValue();
 		
-		//int --> String
+		//Integer --> String
+		Integer integer5 = new Integer(10);
+		String s5 = integer5.toString();
 		
-		//int --> String
+		//String --> Integer
+		String s6 = new String("123");
+		Integer integer6 = Integer.valueOf(s6);
+		
 	}
 }
